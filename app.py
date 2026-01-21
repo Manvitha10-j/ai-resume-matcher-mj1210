e2e13c1cc9889b2fb3315b7ad8bf8b8828f0be30
Manvitha10-j <akkinenimahi1820@gmail.com>
2026-01-21 10:16:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.set_page_config(page_title="Test", layout="centered")
-st.markdown("# ✅ Streamlit is working")
-st.text("If you see this, your setup is correct.")
+st.title("AI Resume Matcher")
+st.write("Setup successful")
+
+
 
 
